b4d0b11f7b5adc41c4bfd72bf37f9cfbe869b1d2
= <=>
2021-02-17 20:54:36 +0100

    Fixed HUD level display and cleared old unused stuff

--- HUD/CharacterUI.gd
+++ HUD/CharacterUI.gd
@@ -12,6 +12,7 @@ var start_experience = 0
 
 onready var hpBar = $HPBarRect
 onready var xpBar = $XPBarRect
+onready var lvlLabel = $LevelLabel
 
 signal current_level_changed
 signal current_experience_changed
@@ -32,6 +33,7 @@ func set_current_experience(value: int):
 	current_level = int(XP_GROWTH * sqrt(current_experience))
 	start_experience = pow(float(current_level) / XP_GROWTH, 2.0)
 	end_experience = pow(float(current_level + 1) / XP_GROWTH, 2.0)
+	lvlLabel.set_text(str(current_level + 1))	
 	if xpBar != null:
 		xpBar.set_value(get_xp_percentage())
 	logForTesting()
@@ -40,9 +42,10 @@ func _ready():
 	self.max_health = PlayerStats.max_health
 	self.health = PlayerStats.health
 	self.current_experience = PlayerStats.total_experience
-	self.current_level = PlayerStats.current_level	
+	self.current_level = PlayerStats.current_level
 	hpBar.set_value(get_health_percentage())
 	xpBar.set_value(get_xp_percentage())
+	lvlLabel.set_text(str(current_level))
 	PlayerStats.connect("health_changed", self, "set_health")
 	PlayerStats.connect("experience_changed", self, "set_current_experience")
 
@@ -57,3 +60,4 @@ func logForTesting() -> void:
 	print("current_xp: " + str(current_experience))	
 	print("current_level: " + str(current_level))
 	print("xp to next lv: " + str(end_experience - current_experience))
+	print("xp percentage: " + str(get_xp_percentage()))
